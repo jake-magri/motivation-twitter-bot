cb0ba5881fb79b7c73b64a5865ef2e091198d8e0
Butter <jmagri1@uncc.edu>
2024-12-16 23:03:39 -0500

    Tweepy is successfully installed and ready to use!

---
+++ twitter-bot.py
@@ -0,0 +1,24 @@
+import tweepy
+
+
+print("Tweepy is successfully installed and ready to use!")
+
+# Replace with your X API credentials
+
+# client_id = "your_client_id"
+
+# client_secret = "your_client_secret"
+
+# access_token = "your_access_token"
+
+# access_token_secret = "your_access_token_secret"
+
+
+
+# client = tweepy.Client(client_id, client_secret, access_token, access_token_secret)
+
+
+
+# response = client.create_tweet(text="This is an automated tweet using the X API with Python!") [1, 2, 5]
+
+# print(response) 
